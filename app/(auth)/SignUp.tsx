deb7c410b5856e41cebcd6b37f760b1f9db8b5d9
rbrunob <bruno.bragaramos@gmail.com>
2024-07-03 22:35:35 -0300

    auth screen front end

--- app/(auth)/SignUp.tsx
+++ app/(auth)/SignUp.tsx
@@ -1,11 +1,83 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Image, ScrollView, Text, View } from 'react-native'
+
+import { images } from '../../constants';
+import FormField from '@/components/FormField';
+import { useState } from 'react';
+import Button from '@/components/Button';
+import { Link } from 'expo-router';
 
 const SignUp = () => {
+  const [form, setForm] = useState({
+    username: '',
+    email: '',
+    password: ''
+  })
+
+  const [isSubmitting, setisSubmitting] = useState(false)
+
+  const submit = () => {
+
+  }
+
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView>
+        <View className='w-full justify-center min-h-[80vh] h-full px-4 my-6'>
+          <Image
+            source={images.logo}
+            resizeMode='contain'
+            className='w-[115px] h-[35px]'
+          />
+
+          <Text className='text-2xl text-white font-semibold mt-10 font-psemibold'>Login to Aora</Text>
+
+          <FormField
+            title="Username"
+            value={form.username}
+            placeholder='Enter your username'
+            handleChangeText={(event: string) => setForm({ ...form, username: event })}
+            otherStyles="mt-10"
+          />
+
+          <FormField
+            title="Email"
+            value={form.email}
+            placeholder='email@example.com'
+            handleChangeText={(event: string) => setForm({ ...form, email: event })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+
+          <FormField
+            title="Password"
+            value={form.password}
+            placeholder='*******'
+            handleChangeText={(event: string) => setForm({ ...form, password: event })}
+            otherStyles="mt-7"
+          />
+
+          <Button
+            title="Sign In"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+
+          <View className='justify-center pt-5 flex-row gap-2'>
+            <Text className='text-lg text-gray-100 font-pregular'>
+              Have an account already?
+            </Text>
+            <Link
+              href="/SignIn"
+              className='text-lg font-psemibold text-secondary'
+            >
+              Sign In
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
---
+++ components/FormField.tsx
@@ -0,0 +1,46 @@
+import { View, Text, TextInput, Image, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+
+import { icons } from '../constants'
+
+type FormFieldPropsType = {
+    title: string,
+    value: string,
+    placeholder: string,
+    handleChangeText: (text: string) => void,
+    otherStyles: string,
+    keyboardType?: string
+}
+
+const FormField = ({ title, value, placeholder, handleChangeText, otherStyles, keyboardType }: FormFieldPropsType) => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    return (
+        <View className={`space-y-2 ${otherStyles}`}>
+            <Text className='text-base text-gray-100 font-pmedium'>{title}</Text>
+
+            <View className='border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row'>
+                <TextInput
+                    className='flex-1 text-white font-psemibold text-base w-full h-full justify-center items-start'
+                    value={value}
+                    placeholder={placeholder}
+                    placeholderTextColor="#7b7b8b"
+                    onChangeText={handleChangeText}
+                    secureTextEntry={title === 'Password' && !showPassword}
+                />
+
+                {title === 'Password' && (
+                    <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+                        <Image
+                            source={!showPassword ? icons.eye : icons.eyeHide}
+                            className='w-6 h-6'
+                            resizeMode='contain'
+                        />
+                    </TouchableOpacity>
+                )}
+            </View>
+        </View>
+    )
+}
+
+export default FormField
